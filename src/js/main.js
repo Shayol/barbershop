efecaf075e70e586e3395842e6de0528868dd367
mila <vorga88@google.com>
2018-10-01 00:00:41 +0300

    add css, js for mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,28 @@ import menuClose from '../icons/menu-close.svg';
 import advant1 from '../icons/advantage-1-illustration.svg';
 import advant2 from '../icons/advantage-2-illustration.svg';
 import advant3 from '../icons/advantage-3-illustration.svg';
+
+
+window.addEventListener("load", function () {
+    function mobileMenu() {
+        let open = document.querySelector(".js-nav");
+
+        let close = document.querySelector(".js-menu-close");
+
+        if (open && close) {
+            open.addEventListener('click', function () {
+                if (open.className.indexOf("js-nav--closed") != -1) {
+                    open.classList.remove("js-nav--closed");
+                    open.classList.add("js-nav--opened");
+                }
+            });
+            close.addEventListener('click', function (e) {
+                open.classList.add("js-nav--closed");
+                open.classList.remove("js-nav--opened");
+                event.stopPropagation();
+            });
+        }
+    }
+
+    mobileMenu();
+});
