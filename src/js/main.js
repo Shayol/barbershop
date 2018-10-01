ed0d31535a108a5672e5252a99b499f5114f2c69
mila <vorga88@google.com>
2018-10-01 19:24:55 +0300

    scale icons, add css for statistics, features, news blocks

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import menuClose from '../icons/menu-close.svg';
 import advant1 from '../icons/advantage-1-illustration.svg';
 import advant2 from '../icons/advantage-2-illustration.svg';
 import advant3 from '../icons/advantage-3-illustration.svg';
+import featuresTriangle from '../icons/triangle-bottom.svg';
 
 
 window.addEventListener("load", function () {
